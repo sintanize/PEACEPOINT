8a11ed09f2c8f12cb7d53547804fb76a155ace3f
sintanize <sintanize1@gmail.com>
2026-07-24 22:59:54 -0400

    Add main.dart for PeacePoint application

---
+++ lib/screens/lib/main.dart
@@ -0,0 +1,11 @@
+// PeacePoint Main App
+
+void main() {
+  runApp(PeacePoint());
+}
+
+class PeacePoint {
+  String name = "PeacePoint";
+
+  String message = "Every small step forward matters.";
+}
