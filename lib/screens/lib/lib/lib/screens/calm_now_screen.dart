86efda478a50378b36c4a0e9d1c3ad4fc36976bd
sintanize <sintanize1@gmail.com>
2026-07-24 23:02:25 -0400

    Add Calm Now Screen for stress relief exercises
    Introduced Calm Now Screen to assist users in managing stress.

---
+++ lib/screens/lib/lib/lib/screens/calm_now_screen.dart
@@ -0,0 +1,17 @@
+// PeacePoint Calm Now Screen
+//
+// Helps users calm down during stressful moments.
+
+class CalmNowScreen {
+  String title = "Calm Now";
+
+  String message =
+      "Take a moment. Breathe slowly. You are here.";
+
+  List<String> exercises = [
+    "Deep Breathing",
+    "Grounding Exercise",
+    "Relaxation",
+    "Calming Sounds",
+  ];
+}
